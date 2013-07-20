9f476a5af5ce5c97b82e2fd3229abaf76f4c466d
Satoshi Ohki <roothybrid7@gmail.com>
2013-07-20 15:48:57 +0900

    Create project

---
+++ RHObjCExt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RHObjCExt
+//
+//  Created by 大木 聡 on 2013/07/20.
+//  Copyright (c) 2013年 roothybrid7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RHObjCExtTests/RHObjCExtTests.h
@@ -0,0 +1,13 @@
+//
+//  RHObjCExtTests.h
+//  RHObjCExtTests
+//
+//  Created by 大木 聡 on 2013/07/20.
+//  Copyright (c) 2013年 roothybrid7. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RHObjCExtTests : SenTestCase
+
+@end
